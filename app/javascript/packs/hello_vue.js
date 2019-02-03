ea2173e0f5b3e9f9524be0c42f9a65d60b545b11
yutainoue <inoue_yuta@nexway.co.jp>
2019-02-03 17:25:02 +0900

    本文をマークダウン表示できるようになった

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,10 +1,16 @@
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 import axios from './axios'
+import _ from 'lodash'
+import marked from 'marked'
 import qs from 'qs'
 
+
+
 var save = new Vue({
-  data: {a: 0},
+  data: {
+    a: 0
+  },
   created: function() {
     setInterval(() => { this.a-- }, 5000)
   },
@@ -13,9 +19,26 @@ var save = new Vue({
       if (c <= 0) {
         axios.put('/memo/1',{
           title: document.querySelector("[v-model='title']").value,
-          text:  document.querySelector("[v-model='text']").value
+          text:  document.querySelector(".editor>textarea").value
         })  
       }
     }
+  }
+})
+
+new Vue({
+  el: '.editor',
+  data: {
+    input: document.querySelector(".editor>textarea").value
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input)
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
   }
 })
